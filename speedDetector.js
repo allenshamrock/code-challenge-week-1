a8239f26e41dffcf2098e276cf1323ad158de420
Allen <allenshamrock37@gmail.com>
2023-12-19 15:37:03 +0300

    Modified the file

--- speedDetector.js
+++ speedDetector.js
@@ -7,11 +7,11 @@ const calculateSpeed = function (speed) {
   let demeritPonts = Math.floor((speed - speedlimit) / 5);
 
   if (speed >= 0 && speed < 70) {
-    return "Ok";
+    console.log("Ok");
   } else if (speed >= 70 && speed <= 130) {
-    return `Points:${demeritPonts}`;
+    console.log(`Points:${demeritPonts}`);
   } else if (speed > 130) {
-    return "License suspended";
+    console.log("License suspended");
   }
 };
 // console.log(calculateSpeed(133));prints the output to the console
